e6329ed61da0c66fd9e210b2433203fe83f99919
Lucas Reis <lucasgreis92@gmail.com>
2020-08-02 16:39:17 -0300

    1.0.0

---
+++ src/main/java/br/com/lgrapplications/kafka/dto/OrderDTO.java
@@ -0,0 +1,34 @@
+package br.com.lgrapplications.kafka.dto;
+
+public class OrderDTO {
+
+    private String id;
+
+    private String nome;
+
+    private Long idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Long idade) {
+        this.idade = idade;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
